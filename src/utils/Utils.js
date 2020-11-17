aedf3ad39106334aca23de9ab5a330b99699a7bb
Kiselyov <a6.kiselyov@itransition.com>
2020-11-17 19:06:08 +0300

    finish

---
+++ src/utils/Utils.js
@@ -0,0 +1,37 @@
+export const successData = {
+  totalCount: 5,
+  page:       1,
+  pageSize:   20,
+  parentId:   104000133,
+  items:
+              [{
+                id:    104000134,
+                name:  "Элемент1",
+                index: "010010010010"
+              }, {
+                id:    104000135,
+                name:  "Элемент2",
+                index: "010010010020"
+              }, {
+                id:    104000136,
+                name:  "Элемент3",
+                index: "010010010020"
+              }, {
+                id:    104000137,
+                name:  "Элемент4",
+                index: "010010010020"
+              }, {
+                id:    104000138,
+                name:  "Элемент5",
+                index: "010010010020"
+              }]
+}
+
+export const errorData = {
+  error: 'произошла ошибка, попробуйте перезагрузить страницу'
+}
+export const buttonText = {
+  send:   'Отправить',
+  fill:   'Заполнить форму',
+  change: 'Изменить запрос'
+}
